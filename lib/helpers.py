1645a97552b7b81986e85d4d7a07395c109b195b
Stefan Hendricks <stefan.hendricks@awi.de>
2014-12-01 16:13:16 +0100

    helpers.py Update

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+
 class EmptyObject():
     """ Container for arbitrary python object"""
     pass
+
+
+def timedelta_str(td):
+    """
+    Solve an issue with negative timedelta strings
+    e.g. '-0:50:00' instead of '-1 day, 23:10:00'
+    returns unchanged str for positive timedeltas
+    """
+    if td.days < 0:
+        return '-' + str(datetime.timedelta() - td)
+    return str(td)
